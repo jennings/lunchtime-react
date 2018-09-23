c233789e406d670eeb48523a8b5d0d87f5826cae
Stephen Jennings <stephen.g.jennings@gmail.com>
2018-09-23 15:23:20 -0700

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,30 +1,115 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+class Store {
+  constructor() {
+    firebase.initializeApp({
+      authDomain: 'lunchtime-react-95b31.firebaseapp.com',
+      projectId: 'lunchtime-react-95b31',
+    });
+
+    this.db = firebase.firestore();
+
+    this.db.settings({
+      timestampsInSnapshots: true,
+    });
+  }
+
+  listenForDestinations(observer) {
+    const unsubscribe = this.db
+      .collection('destinations')
+      .onSnapshot(querySnapshot => {
+        const changes = querySnapshot.docChanges();
+        for (const change of changes) {
+          switch (change.type) {
+            case 'added':
+              observer.onAdded &&
+                observer.onAdded({id: change.doc.id, ...change.doc.data()});
+              break;
+            case 'modified':
+              observer.onModified &&
+                observer.onModified({id: change.doc.id, ...change.doc.data()});
+              break;
+            case 'removed':
+              observer.onRemoved && observer.onRemoved({id: change.doc.id});
+              break;
+            default:
+              console.log('unrecognized change type', change);
+          }
+        }
+      });
+    return {unsubscribe};
+  }
+
+  async createDestination(dest) {
+    const docRef = await this.db.collection('destinations').add(dest);
+    return {id: docRef.id, ...docRef.data()};
+  }
+
+  deleteDestination(id) {
+    return this.db
+      .collection('destinations')
+      .doc(id)
+      .delete();
+  }
+}
 
 class App extends Component {
   constructor() {
     super();
+    this.store = new Store();
+
     this.state = {
-      destinations: [{name: "Teddy's"}, {name: 'Chipotle'}, {name: "Nina's"}],
+      loading: true,
     };
 
     this.onDestinationCreate = this.onDestinationCreate.bind(this);
     this.onDestinationDelete = this.onDestinationDelete.bind(this);
   }
 
+  async componentDidMount() {
+    const observer = {
+      onAdded: dest => {
+        const destinations = [...(this.state.destinations || []), dest];
+        this.setState({destinations, loading: false});
+      },
+      onModified: dest => {
+        const destinations = this.state.destinations(d => {
+          return d.id === dest.id ? dest : d;
+        });
+        this.setState({destinations, loading: false});
+      },
+      onRemoved: dest => {
+        const destinations = this.state.destinations.filter(
+          d => d.id !== dest.id,
+        );
+        this.setState({destinations, loading: false});
+      },
+    };
+    this.unsubscribe = await this.store.listenForDestinations(observer);
+  }
+
   onDestinationCreate(dest) {
-    const destinations = [...this.state.destinations, dest];
-    this.setState({destinations});
+    this.store.createDestination(dest);
   }
 
   onDestinationDelete(dest) {
-    console.log('deleting', dest);
-    const destinations = this.state.destinations.filter(d => d !== dest);
-    this.setState({destinations});
+    this.store.deleteDestination(dest.id);
   }
 
   render() {
+    const list = this.state.loading ? (
+      <div>Loading...</div>
+    ) : (
+      <DestinationList
+        destinations={this.state.destinations}
+        onCreate={this.onDestinationCreate}
+        onDelete={this.onDestinationDelete}
+      />
+    );
     return (
       <div className="App">
         <header className="App-header">
@@ -32,13 +117,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
 
-        <div className="App-body">
-          <DestinationList
-            destinations={this.state.destinations}
-            onCreate={this.onDestinationCreate}
-            onDelete={this.onDestinationDelete}
-          />
-        </div>
+        <div className="App-body">{list}</div>
 
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
