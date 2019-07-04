d5f1ba418bf576345cf6f960ab4f66f7a4d2365d
Stephen Jennings <stephen.g.jennings@gmail.com>
2019-07-04 15:56:12 -0700

    convert SignInDialog to a function

--- src/SignInDialog.js
+++ src/SignInDialog.js
@@ -1,33 +1,26 @@
-import React, {Component} from 'react';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import {FirebaseAuth} from 'react-firebaseui';
+import React from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { FirebaseAuth } from "react-firebaseui";
 
-class SignInDialog extends Component {
-  constructor(props) {
-    super(props);
-    this.firebaseUiConfig = {
-      signInFlow: 'popup',
-      signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-      ],
-      callbacks: {
-        signInSuccessWithAuthResult: result => {
-          this.props.authService.signIn(result.user);
-        },
-      },
-    };
-  }
+export default function SignInDialog({ authService }) {
+  const firebaseUiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: result => {
+        authService.signIn(result.user);
+      }
+    }
+  };
 
-  render() {
-    return (
-      <FirebaseAuth
-        uiConfig={this.firebaseUiConfig}
-        firebaseAuth={this.props.authService.firebaseAuth}
-      />
-    );
-  }
+  return (
+    <FirebaseAuth
+      uiConfig={firebaseUiConfig}
+      firebaseAuth={authService.firebaseAuth}
+    />
+  );
 }
-
-export default SignInDialog;
