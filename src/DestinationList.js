c8bba7d74d4c625d83f9e2d8eed15beb8e3f9a4a
Stephen Jennings <stephen.g.jennings@gmail.com>
2019-07-04 15:49:39 -0700

    convert DestinationList to functions with hooks

--- src/DestinationList.js
+++ src/DestinationList.js
@@ -1,84 +1,61 @@
-import React, {Component} from 'react';
-import {localeCompareWithSelector} from './util/compare';
+import React from "react";
+import { useState } from "react";
+import { localeCompareWithSelector } from "./util/compare";
 
-class DestinationList extends Component {
-  constructor(props) {
-    super(props);
-    this.handleNewDestination = this.handleNewDestination.bind(this);
-    this.handleDeleteDestination = this.handleDeleteDestination.bind(this);
-  }
+export default function DestinationList({ destinations, onCreate, onDelete }) {
+  const destinationItems = destinations
+    .slice()
+    .sort(localeCompareWithSelector(d => d.name))
+    .map(d => (
+      <li key={d.name}>
+        <button onClick={() => onDelete(d)}>delete</button>
+        {" " + d.name}
+      </li>
+    ));
 
-  handleNewDestination(dest) {
-    this.props.onCreate(dest);
-  }
-
-  handleDeleteDestination(dest) {
-    this.props.onDelete(dest);
-  }
-
-  render() {
-    const destinationItems = this.props.destinations
-      .slice()
-      .sort(localeCompareWithSelector(d => d.name))
-      .map(d => (
-        <li key={d.name}>
-          <button onClick={() => this.handleDeleteDestination(d)}>
-            delete
-          </button>
-          {' ' + d.name}
-        </li>
-      ));
-
-    return (
-      <div>
-        <ul>{destinationItems}</ul>
-        Add new:
-        <br />
-        <DestinationAdder onAdd={this.handleNewDestination} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <ul>{destinationItems}</ul>
+      Add new:
+      <br />
+      <DestinationAdder onAdd={onCreate} />
+    </div>
+  );
 }
 
-class DestinationAdder extends Component {
-  constructor(props) {
-    super();
-    this.state = {value: props.value || ''};
-    this.handleInputKeyPress = this.handleInputKeyPress.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleValueChanged = this.handleValueChanged.bind(this);
-  }
+function DestinationAdder({ value, onAdd }) {
+  const [text, setText] = useState(value);
 
-  handleSubmit() {
-    this.props.onAdd({name: this.state.value});
-    this.setState({value: ''});
-    this.valueInput.focus();
-  }
+  let inputElement = null;
 
-  handleValueChanged(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleInputKeyPress(event) {
-    if (event.key === 'Enter') {
-      this.handleSubmit();
+  const handleSubmit = () => {
+    onAdd({ name: text });
+    setText("");
+    if (inputElement) {
+      inputElement.focus();
     }
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <label>Name</label>
-        <input
-          ref={input => (this.valueInput = input)}
-          value={this.state.value}
-          onChange={this.handleValueChanged}
-          onKeyPress={this.handleInputKeyPress}
-        />
-        <button onClick={this.handleSubmit}>Add</button>
-      </div>
-    );
-  }
+  const handleValueChanged = event => {
+    setText(event.target.value);
+  };
+
+  const handleInputKeyPress = event => {
+    if (event.key === "Enter") {
+      handleSubmit();
+    }
+  };
+
+  return (
+    <div>
+      <label>Name</label>
+      <input
+        ref={i => (inputElement = i)}
+        value={text}
+        onChange={handleValueChanged}
+        onKeyPress={handleInputKeyPress}
+      />
+      <button onClick={handleSubmit}>Add</button>
+    </div>
+  );
 }
-
-export default DestinationList;
